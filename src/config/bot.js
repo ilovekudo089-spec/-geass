9e8cdd96c7c9127ecf4efff81d2a183f89908839
ilovekudo089-spec <ilovekudo089@gmail.com>
2026-04-15 23:52:49 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "yugi own's me",
+        name: "kudo own's me",
         // Activity type number (0 = Playing).
         type: 2, 
       },
